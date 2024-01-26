ba730371ecd50b2656782dcb8318a12e3f4d75ac
xyz <37243611+PowerShop@users.noreply.github.com>
2024-01-26 22:31:32 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    PTKidsBITRobot.ForwardTIME(
+    500,
+    50,
+    100,
+    0.02,
+    0
+    )
+    PTKidsBITRobot.ForwardLINE(
+    Find_Line.Center,
+    50,
+    100,
+    0.02,
+    0
+    )
+    PTKidsBITRobot.ForwardTIME(
+    100,
+    50,
+    100,
+    0.02,
+    0
+    )
+    PTKidsBITRobot.motorStop()
+    basic.pause(100)
+})
+PTKidsBITRobot.SensorCalibrate()
+PTKidsBITRobot.servoWrite(Servo_Write.P8, 110)
+PTKidsBITRobot.servoWrite(Servo_Write.P12, 115)
+PTKidsBITRobot.motorWrite(Motor_Write.Motor_Left, 0)
+PTKidsBITRobot.motorWrite(Motor_Write.Motor_Right, 0)
