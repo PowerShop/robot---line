51eb764606654f75c050f465f9e5c46889681c5b
xyz <37243611+PowerShop@users.noreply.github.com>
2024-01-28 12:27:29 +0700

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,29 +1,40 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("A")
+input.onButtonPressed(Button.B, function () {
     PTKidsBITRobot.ForwardTIME(
     500,
     50,
     100,
-    0.02,
+    0.01,
     0
     )
     PTKidsBITRobot.ForwardLINE(
     Find_Line.Center,
     50,
     100,
-    0.02,
+    0.01,
     0
     )
     PTKidsBITRobot.ForwardTIME(
     100,
     50,
     100,
-    0.02,
+    0.01,
     0
     )
     PTKidsBITRobot.motorStop()
     basic.pause(100)
+    PTKidsBITRobot.TurnLINE(
+    Turn_Line.Left,
+    50,
+    Turn_ADC.ADC2,
+    200
+    )
+    PTKidsBITRobot.motorStop()
+    basic.pause(100)
+    PTKidsBITRobot.servoWrite(Servo_Write.P8, 110)
+    basic.pause(100)
 })
+basic.showIcon(IconNames.Yes)
+PTKidsBITRobot.setSensorColorAll(LED_Color.Green)
 PTKidsBITRobot.SensorCalibrate()
-PTKidsBITRobot.servoWrite(Servo_Write.P8, 110)
-PTKidsBITRobot.servoWrite(Servo_Write.P12, 115)
+PTKidsBITRobot.servoWrite(Servo_Write.P8, 15)
+PTKidsBITRobot.servoWrite(Servo_Write.P12, 170)
